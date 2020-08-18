640f8a5cf4ccabb4eddfaf89576a9658b18d9848
Vipul Aggarwal <aggarwal.vipul21@gmail.com>
2020-08-18 06:53:40 -0400

    Added code in app.js file to load the table based on data in data.js file

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,21 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+console.log(tableData[0].city);
+
+var table_body=d3.select("tbody")
+
+tableData.forEach((ufo_data) =>{
+   
+   console.log(ufo_data);
+   var table_row= table_body.append("tr");
+
+   Object.entries(ufo_data).forEach(([key,value])=>{
+    var table_col= table_row.append("td");
+    table_col.text(value);
+
+   });
+
+});
+
