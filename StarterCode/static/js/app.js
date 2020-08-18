3c2bab5116cf74af8e840c0b7253d116990d213f
Vipul Aggarwal <aggarwal.vipul21@gmail.com>
2020-08-18 08:18:38 -0400

    Added code to find records that match the date and print in console

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,13 +3,13 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
-console.log(tableData[0].city);
+//console.log(tableData[0].city);
 
 var table_body=d3.select("tbody")
 
 tableData.forEach((ufo_data) =>{
    
-   console.log(ufo_data);
+   //console.log(ufo_data);
    var table_row= table_body.append("tr");
 
    Object.entries(ufo_data).forEach(([key,value])=>{
@@ -20,3 +20,44 @@ tableData.forEach((ufo_data) =>{
 
 });
 
+function filterbydate(selected_date,ufo_sighting){
+    console.log(selected_date);
+    //console.log(ufo_sighting.datetime);
+    return ufo_sighting.datetime===selected_date;
+
+};
+
+var date_filter_btn=d3.select("#filter-btn");
+
+var date_filter_form=d3.select("#form-date-filter");
+
+//console.log(date_filter_btn.text());
+
+//Create event handlers
+
+date_filter_btn.on("click",submit_date_filter);
+date_filter_form.on("submit",submit_date_filter);
+
+// Event handler function for the date filter
+function submit_date_filter() {
+
+    //console.log(d3.event.target);
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var selected_date = inputElement.property("value");
+
+    //console.log(tableData);
+    console.log(selected_date);
+
+    var filtered_sighting_date=tableData.filter(filterbydate.bind(this,selected_date));
+    //(ufo_data => ufo_data.datetime===selected_date)
+    //filterbydate(inputValue));
+
+    console.log(filtered_sighting_date);
+};
