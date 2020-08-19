2a7e75365f24baa78bd9e891ff8bc32d7d30d20f
Vipul Aggarwal <aggarwal.vipul21@gmail.com>
2020-08-18 23:02:05 -0400

    Added code to empty data in table on click of filter and call function to load data based on filtered data

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -21,14 +21,19 @@ tableData.forEach((ufo_data) =>{
 });
 
 function filterbydate(selected_date,ufo_sighting){
-    console.log(selected_date);
+    //console.log(selected_date);
     //console.log(ufo_sighting.datetime);
     return ufo_sighting.datetime===selected_date;
 
 };
 
+// Select the input element and get the raw HTML node
+var inputElement = d3.select("#datetime");
+
+//
 var date_filter_btn=d3.select("#filter-btn");
 
+//
 var date_filter_form=d3.select("#form-date-filter");
 
 //console.log(date_filter_btn.text());
@@ -55,9 +60,22 @@ function submit_date_filter() {
     //console.log(tableData);
     console.log(selected_date);
 
-    var filtered_sighting_date=tableData.filter(filterbydate.bind(this,selected_date));
-    //(ufo_data => ufo_data.datetime===selected_date)
-    //filterbydate(inputValue));
+    if (selected_date){
 
-    console.log(filtered_sighting_date);
+    var filtered_by_sighting_date=tableData.filter(filterbydate.bind(this,selected_date));
+    
+    //Set the table body to blank
+    d3.select("tbody").html("");
+    console.log(filtered_by_sighting_date);
+
+    buildfilteredtable(filtered_by_sighting_date);
+    }
+    else {
+        alert("Please enter date in format m/d/yyyy");
+        //console.log("In Else part")
+    }
+
+    
 };
+
+function buildfilteredtable(data){}
